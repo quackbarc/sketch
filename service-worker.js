b41e176abd5da6d7cade69d4f1bb28045a6c7c7c
quackbarc <49148994+quackbarc@users.noreply.github.com>
2024-03-04 06:41:21 +0800

    deploy a temp build with a safari bugfix

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","204544d59892d7cc1b4c7a7211b325bd"]];
+var precacheConfig = [["index.html","fec20e9b43617f95cddc4328ea11eb8b"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
