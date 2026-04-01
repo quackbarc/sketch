9ea98171ae14e4a4b5fbb2e1257744890066fac2
quackbarc <49148994+quackbarc@users.noreply.github.com>
2026-04-01 23:38:28 +0800

    deploy from 147b04b (april fools, brush)

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","81f3ae5ae11704ca7f40a09890cc64ea"]];
+var precacheConfig = [["index.html","899aa001c5f4971aab01b6a60772841f"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
